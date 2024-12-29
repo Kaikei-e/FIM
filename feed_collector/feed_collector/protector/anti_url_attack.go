efa0e6e28b931f96deb13408440591d963f4e689
Kaikei-e <e_kaikei@protonmail.com>
2024-12-29 16:30:58 +0900

    fix: improve URL validation and error logging in AntiURLAttack function

--- feed_collector/feed_collector/protector/anti_url_attack.go
+++ feed_collector/feed_collector/protector/anti_url_attack.go
@@ -1,32 +1,36 @@
 package protector
 
 import (
+	"errors"
+	"feed_collector/slogger"
 	"net/url"
-	"time"
 
 	"github.com/doyensec/safeurl"
 )
 
-func AntiURLAttack(link string) (*url.URL, error) {
-	ul, err := url.Parse(link)
+func AntiURLAttack(link string, client *safeurl.WrappedClient) (*url.URL, error) {
+	parsedURL, err := url.Parse(link)
 	if err != nil {
+		slogger.Logger.Error("Failed to parse the URL.", "Caused by", err)
 		return nil, err
 	}
 
-	dur := time.Duration(2) * time.Second
+	if parsedURL.Host == "" || parsedURL.Scheme == "" {
+		slogger.Logger.Error("Invalid URL.", "URL", link)
+		return nil, errors.New("host is empty and url is not valid")
+	}
 
-	config := safeurl.GetConfigBuilder().
-		EnableIPv6(true).
-		SetTimeout(dur).
-		SetAllowedPorts(80, 443).
-		Build()
+	if parsedURL.RawQuery != "" {
+		slogger.Logger.Error("URL contains query parameters.", "URL", link)
+		return nil, errors.New("url contains query parameters")
+	}
 
-	cl := safeurl.Client(config)
-	resp, err := cl.Get(link)
+	resp, err := client.Get(parsedURL.String())
 	if err != nil {
+		slogger.Logger.Error("Failed to get the URL.", "Caused by", err)
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-	return ul, nil
+	return parsedURL, nil
 }
