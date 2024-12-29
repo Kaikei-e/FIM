d81acdb21ae9154cb05dcde016e8819b7ef86d73
Kaikei-e <e_kaikei@protonmail.com>
2024-12-29 15:34:42 +0900

    test: add unit tests for CollectRSSFeed function with valid and invalid RSS links

---
+++ feed_collector/feed_collector/client/collect_rss_feed_test.go
@@ -0,0 +1,93 @@
+package client
+
+import (
+	"net/url"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/doyensec/safeurl"
+	"github.com/mmcdole/gofeed"
+)
+
+var dur = time.Duration(2) * time.Second
+var safeurlClient = safeurl.GetConfigBuilder().
+	EnableIPv6(true).
+	SetTimeout(dur).
+	SetAllowedPorts(80, 443).
+	Build()
+
+func TestCollectRSSFeed(t *testing.T) {
+	type args struct {
+		rssLink url.URL
+		cl      *safeurl.WrappedClient
+	}
+	type want struct {
+		feed gofeed.Feed
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    want
+		wantErr bool
+	}{
+		{
+			name: "collect rss feed",
+			args: args{
+				rssLink: url.URL{
+					Scheme: "https",
+					Host:   "lorem-rss.herokuapp.com",
+					Path:   "/feed",
+				},
+				cl: safeurl.Client(safeurlClient),
+			},
+			want: want{
+				feed: gofeed.Feed{
+					Title: "Lorem ipsum feed for an interval of 1 minutes with 10 item(s)",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid rss feed link",
+			args: args{
+				rssLink: url.URL{
+					Scheme: "https",
+					Host:   "invalid-url",
+				},
+				cl: safeurl.Client(safeurlClient),
+			},
+			want: want{
+				feed: gofeed.Feed{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "malicious rss feed link",
+			args: args{
+				rssLink: url.URL{
+					Scheme: "https",
+					Host:   "malicious-url.com",
+				},
+				cl: safeurl.Client(safeurlClient),
+			},
+			want: want{
+				feed: gofeed.Feed{},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := CollectRSSFeed(tt.args.rssLink, tt.args.cl)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CollectRSSFeed() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CollectRSSFeed() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
