a1049a6b7cafb3b8f4c3efb936587d8822f35b79
Kaikei-e <e_kaikei@protonmail.com>
2024-12-29 16:49:25 +0900

    fix: enhance error logging and URL handling in CollectRSSFeed function

--- feed_collector/feed_collector/client/collect_rss_feed.go
+++ feed_collector/feed_collector/client/collect_rss_feed.go
@@ -2,7 +2,7 @@ package client
 
 import (
 	"feed_collector/protector"
-	"log"
+	"feed_collector/slogger"
 	"net/url"
 
 	"github.com/doyensec/safeurl"
@@ -10,18 +10,34 @@ import (
 )
 
 func CollectRSSFeed(rssLink url.URL, cl *safeurl.WrappedClient) (*gofeed.Feed, error) {
-	targetLink, err := protector.AntiURLAttack(rssLink.String())
+	linkPath := rssLink.Path
+	rssLink.Path = ""
+
+	targetLink, err := protector.AntiURLAttack(rssLink.String(), cl)
 	if err != nil {
-		log.Println("This link is potentially malicious")
+		slogger.Logger.Error("Failed to protect against URL attack.", "Caused by", err)
+		return nil, err
+	}
+
+	fromattedPath, err := url.JoinPath(targetLink.String(), linkPath)
+	if err != nil {
+		slogger.Logger.Error("Failed to join the path.", "Caused by", err)
+		return nil, err
+	}
+
+	reParsedURL, err := url.Parse(fromattedPath)
+	if err != nil {
+		slogger.Logger.Error("Failed to parse the URL.", "Caused by", err)
 		return nil, err
 	}
 
 	fp := gofeed.NewParser()
-
-	feed, err := fp.ParseURL(targetLink.String())
+	feed, err := fp.ParseURL(reParsedURL.String())
 	if err != nil {
+		slogger.Logger.Error("Failed to parse the feed.", "Caused by", err)
 		return nil, err
 	}
 
+	slogger.Logger.Info("Successfully collected the feed.", "Feed", feed.Title)
 	return feed, nil
 }
