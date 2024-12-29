b2380fc48ded342ae4df0d4e3a92909e67fc286d
Kaikei-e <e_kaikei@protonmail.com>
2024-12-29 17:20:43 +0900

    feat: add CollectSingleFeed function to handle single feed collection and error logging

---
+++ feed_collector/feed_collector/router/apiV1/collect_feed.go
@@ -0,0 +1,30 @@
+package apiv1
+
+import (
+	"feed_collector/client"
+	"feed_collector/protector"
+	"feed_collector/slogger"
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+)
+
+func CollectSingleFeed(g *echo.Context) error {
+	cl := protector.InitClient()
+
+	targetURL, err := protector.AntiURLAttack("https://www.example.com/rss", cl)
+	if err != nil {
+		slogger.Logger.Error("Failed to parse the URL.", "Caused by", err)
+		return err
+	}
+
+	feed, err := client.CollectRSSFeed(*targetURL, cl)
+	if err != nil {
+		slogger.Logger.Error("Failed to collect the feed.", "Caused by", err)
+		return err
+	}
+
+	fmt.Println(feed.Link)
+
+	return nil
+}
