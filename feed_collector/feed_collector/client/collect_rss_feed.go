4f15084749eddd526e29fd26a4bcaadd504171ed
Kaikei-e <e_kaikei@protonmail.com>
2024-12-29 17:18:19 +0900

    fix: simplify URL handling in CollectRSSFeed function by removing unnecessary path manipulation

--- feed_collector/feed_collector/client/collect_rss_feed.go
+++ feed_collector/feed_collector/client/collect_rss_feed.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-	"feed_collector/protector"
 	"feed_collector/slogger"
 	"net/url"
 
@@ -10,29 +9,8 @@ import (
 )
 
 func CollectRSSFeed(rssLink url.URL, cl *safeurl.WrappedClient) (*gofeed.Feed, error) {
-	linkPath := rssLink.Path
-	rssLink.Path = ""
-
-	targetLink, err := protector.AntiURLAttack(rssLink.String(), cl)
-	if err != nil {
-		slogger.Logger.Error("Failed to protect against URL attack.", "Caused by", err)
-		return nil, err
-	}
-
-	fromattedPath, err := url.JoinPath(targetLink.String(), linkPath)
-	if err != nil {
-		slogger.Logger.Error("Failed to join the path.", "Caused by", err)
-		return nil, err
-	}
-
-	reParsedURL, err := url.Parse(fromattedPath)
-	if err != nil {
-		slogger.Logger.Error("Failed to parse the URL.", "Caused by", err)
-		return nil, err
-	}
-
 	fp := gofeed.NewParser()
-	feed, err := fp.ParseURL(reParsedURL.String())
+	feed, err := fp.ParseURL(rssLink.String())
 	if err != nil {
 		slogger.Logger.Error("Failed to parse the feed.", "Caused by", err)
 		return nil, err
