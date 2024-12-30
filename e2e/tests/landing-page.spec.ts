35852490612f727a4ddaa5e1eb45f4ebd6b5dfdd
Kaikei-e <e_kaikei@protonmail.com>
2024-12-31 01:03:05 +0900

    add: implement test for landing page navigation and "Get Started" button click

--- e2e/tests/landing-page.spec.ts
+++ e2e/tests/landing-page.spec.ts
@@ -6,3 +6,11 @@ test('go to landing page', async ({ page }) => {
 
   await expect(element).toBeVisible({ timeout: 1000 });
 })
+
+test('go to landing page and click get started', async ({ page }) => {
+  await page.goto('http://localhost:5173');
+  await page.click('id=get-started');
+  const url = page.url();
+
+  expect(url).toBe('http://localhost:5173/home');
+})
