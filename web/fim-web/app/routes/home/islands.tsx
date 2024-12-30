6da1d08be962b3a5a9a05f087b3ebac6aa8bb30c
Kaikei-e <e_kaikei@protonmail.com>
2024-12-30 16:06:26 +0900

    add: enhance islands component with dashboard insights and additional sections

--- web/fim-web/app/routes/home/islands.tsx
+++ web/fim-web/app/routes/home/islands.tsx
@@ -1,11 +1,27 @@
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-      <div>
+    <div className="h-screen w-screen p-8 bg-slate-200 flex flex-col">
+      <div className="flex flex-col">
         <h1 className="text-2xl font-semibold">Islands</h1>
         <p>
-          Islands is based on the multiple components that have different functinalities.
+          Islands is based on the multiple components that have different
+          functinalities.
         </p>
+        <p>This is the dashboard for your insights.</p>
+      </div>
+      <div className="h-full w-full p-4 grid grid-cols-1 gap-4 sm:grid-cols-2">
+        <div>
+          <p>Search feeds</p>
+        </div>
+        <div>
+          <p>Register feeds</p>
+        </div>
+        <div>
+          <p>Latest feeds</p>
+        </div>
+        <div>
+          <p>Today&apos;s topics</p>
+        </div>
       </div>
     </div>
   );
