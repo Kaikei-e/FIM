3666538c3e9e5f8f9de4a3b3a4e57df1a3c3b89c
Kaikei-e <e_kaikei@protonmail.com>
2024-12-30 15:52:41 +0900

    add: create islands component in home route

---
+++ web/fim-web/app/routes/home/islands.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+      <div>
+        <h1 className="text-2xl font-semibold">Islands</h1>
+        <p>
+          Islands is based on the multiple components that have different functinalities.
+        </p>
+      </div>
+    </div>
+  );
+}
