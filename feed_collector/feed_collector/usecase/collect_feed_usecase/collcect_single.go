4f2842429ae8a0db292ae90089ac451bcec2fa75
Kaikei-e <e_kaikei@protonmail.com>
2024-12-31 17:43:28 +0900

    feat: update CollectSingleFeedUsecase to include database interaction and context support

--- feed_collector/feed_collector/usecase/collect_feed_usecase/collcect_single.go
+++ feed_collector/feed_collector/usecase/collect_feed_usecase/collcect_single.go
@@ -1,13 +1,16 @@
 package collect_feed_usecase
 
 import (
+	"context"
+	"database/sql"
 	"feed_collector/client"
+	"feed_collector/domain/rss_feed"
+	"feed_collector/driver/db_driver"
 	"feed_collector/protector"
 	"feed_collector/slogger"
-	"fmt"
 )
 
-func CollectSingleFeedUsecase(reqBody string) error {
+func CollectSingleFeedUsecase(reqBody string, db *sql.DB, ctx context.Context) error {
 	cl := protector.InitClient()
 
 	targetURL, err := protector.AntiURLAttack(reqBody, cl)
@@ -22,7 +25,19 @@ func CollectSingleFeedUsecase(reqBody string) error {
 		return err
 	}
 
-	fmt.Println(feed.Link)
+	insertingFeed := rss_feed.Feed{
+		URL:           feed.Link,
+		Title:         feed.Title,
+		Description:   feed.Description,
+		Author:        feed.Author.Name,
+		PublishedDate: *feed.PublishedParsed,
+	}
+
+	err = db_driver.Register(insertingFeed, db, ctx)
+	if err != nil {
+		slogger.Logger.Error("Failed to register the feed.", "Caused by", err)
+		return err
+	}
 
 	return nil
 }
