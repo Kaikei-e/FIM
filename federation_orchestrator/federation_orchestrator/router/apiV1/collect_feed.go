2681188ba002b7e43ca044b1c4b4fed2f9149b6e
Kaikei-e <e_kaikei@protonmail.com>
2024-12-31 17:33:30 +0900

    feat: implement BffServer and update CollectSingleFeed function for feed retrieval

--- federation_orchestrator/federation_orchestrator/router/apiV1/collect_feed.go
+++ federation_orchestrator/federation_orchestrator/router/apiV1/collect_feed.go
@@ -2,14 +2,33 @@ package apiv1
 
 import (
 	"context"
+	"errors"
+	feedv1 "federation_orchestrator/gen/schema/feed/v1"
+	"federation_orchestrator/gen/schema/feed/v1/feedv1connect"
 	"federation_orchestrator/slogger"
 	"fmt"
+	"net/http"
+	"time"
 
+	"connectrpc.com/connect"
+	"github.com/doyensec/safeurl"
 	"github.com/labstack/echo/v4"
 )
 
 type targetURL struct {
 	URL string `json:"url"`
+}
+
+type BffServer struct {
+	apiClient     feedv1connect.FeedServiceClient
+	safeurlClient *safeurl.WrappedClient
+}
+
+func NewBffServer(apiClient feedv1connect.FeedServiceClient, safeurlClient *safeurl.WrappedClient) *BffServer {
+	return &BffServer{
+		apiClient:     apiClient,
+		safeurlClient: safeurlClient,
+	}
 }
 
 func CollectSingleFeed(e echo.Context, ctx context.Context) error {
@@ -22,6 +41,34 @@ func CollectSingleFeed(e echo.Context, ctx context.Context) error {
 	}
 
 	fmt.Println(reqString)
+
+	s := feedv1connect.NewFeedServiceClient(http.DefaultClient,
+		"http://10.0.100.30:8080") // TODO: replace with the dynamic address
+
+	reqRPC := connect.NewRequest(&feedv1.GetFeedRequest{
+		FeedUrl: reqString.URL,
+	})
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	_, err = s.GetFeed(ctx, reqRPC)
+	if err != nil {
+		var connectErr *connect.Error
+		if errors.As(err, &connectErr) {
+			switch connectErr.Code() {
+			case connect.CodeNotFound:
+				return e.JSON(http.StatusNotFound, map[string]string{"error": "Feed not found."})
+			case connect.CodeInvalidArgument:
+				return e.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid argument."})
+			default:
+				slogger.Logger.Error("GetFeed RPC error", "error", connectErr)
+				return e.JSON(http.StatusInternalServerError, map[string]string{"error": "Internal Server Error."})
+			}
+		}
+		slogger.Logger.Error("GetFeed RPC unexpected error", "error", err)
+		return e.JSON(http.StatusInternalServerError, map[string]string{"error": "Internal Server Error."})
+	}
 
 	return e.JSON(200, "Successfully collected the feed.")
 }
