9be8b2531bf02b665f162ae0a97d947d0ac60206
Kaikei-e <e_kaikei@protonmail.com>
2024-12-31 23:14:03 +0900

    feat: enhance Register component with styling and add Register button

--- web/fim-web/app/components/islands/register.tsx
+++ web/fim-web/app/components/islands/register.tsx
@@ -4,10 +4,20 @@ interface RegisterProps {
 
 export const Register = (props: RegisterProps) => {
   return (
-    <div>
-      <h1>Register feeds</h1>
-      <p>Register your feeds here.</p>
-      <p>URL: {props.url}</p>
+    <div className="h-full w-full flex-col bg-white p-4 rounded-lg shadow-md">
+      <div>
+        <h1>Register feeds</h1>
+        <p>Register your feeds here.</p>
+        <p>URL: {props.url}</p>
+      </div>
+      <div>
+        <button
+          id="registerButton"
+          className="bg-blue-500 text-white p-2 rounded-lg"
+        >
+          Register
+        </button>
+      </div>
     </div>
   );
 };
