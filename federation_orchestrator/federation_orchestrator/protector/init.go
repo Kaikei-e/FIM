d14603c18c37a3b60af5d0534b080c0df90bcede
Kaikei-e <e_kaikei@protonmail.com>
2024-12-31 17:32:52 +0900

    feat: add InitClient function to configure and initialize safeurl client

---
+++ federation_orchestrator/federation_orchestrator/protector/init.go
@@ -0,0 +1,22 @@
+package protector
+
+import (
+	"time"
+
+	"github.com/doyensec/safeurl"
+)
+
+func InitClient() *safeurl.WrappedClient {
+
+	dur := time.Duration(1) * time.Second
+	config := safeurl.GetConfigBuilder().
+		EnableIPv6(true).
+		SetAllowedSchemes("https").
+		SetTimeout(dur).
+		SetAllowedPorts(80, 443).
+		Build()
+
+	safeurlClient := safeurl.Client(config)
+
+	return safeurlClient
+}
