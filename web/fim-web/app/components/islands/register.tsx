69ba0738254c86d6d72ba476da2fd6afa0a4e0d8
Kaikei-e <e_kaikei@protonmail.com>
2025-01-01 01:06:49 +0900

    feat: refactor Register component to manage feed URL state and handle registration

--- web/fim-web/app/components/islands/register.tsx
+++ web/fim-web/app/components/islands/register.tsx
@@ -1,22 +1,44 @@
-interface RegisterProps {
-  url: string;
-}
+import { useState } from "react";
+import { registerSingleFeed } from "~/lib/driver/register_single_feed";
 
-export const Register = (props: RegisterProps) => {
+// interface RegisterProps {
+//   url: string;
+// }
+
+export const Register = () => {
+  const [url, setUrl] = useState("");
+
   return (
     <div className="h-full w-full flex-col bg-white p-4 rounded-lg shadow-md">
       <div>
         <h1>Register feeds</h1>
         <p>Register your feeds here.</p>
-        <p>URL: {props.url}</p>
       </div>
-      <div>
-        <button
-          id="registerButton"
-          className="bg-blue-500 text-white p-2 rounded-lg"
-        >
-          Register
-        </button>
+      <div className="flex flex-col w-1/3 justify-center">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <label htmlFor="feedUrlInputField" className="mt-4">Feed URL</label>
+          <input
+            id="feedUrlInputField"
+            type="url"
+            className="border border-gray-300 mt-4 p-2 rounded-lg"
+            placeholder="Enter feed URL"
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button
+            id="registerButton"
+            className="bg-indigo-400 text-white mt-4 p-2 rounded-lg"
+            onClick={async () => {
+              console.log("Register button clicked");
+              const resJson = await registerSingleFeed(url);
+
+              if (resJson) {
+                alert("The feed has been registered successfully");
+              }
+            }}
+          >
+            Register
+          </button>
+        </form>
       </div>
     </div>
   );
